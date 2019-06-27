70f847955c2a43fd7592562e0ad3e56c8fa40fa8
alvaro <alvaro@192.168.1.39>
2019-06-27 22:32:03 +0200

    1.0.1 - 27/6/2019

---
+++ game/src/layer/LayerStack.java
@@ -0,0 +1,30 @@
+package layer;
+
+import java.util.ArrayList;
+
+public class LayerStack {
+
+	public ArrayList<Layer> layers = new ArrayList<Layer>();
+	private int overlayCount = 0;
+
+	public void pushLayer(Layer layer) {
+		layers.add(overlayCount, layer);
+		overlayCount++;
+	}
+
+	public void pushOverlay(Layer layer) {
+		layers.add(layer);
+	}
+
+	public void popLayer() {
+		layers.get(overlayCount);
+		layers.remove(overlayCount);
+		overlayCount--;
+	}
+
+	public void popOverlay() {
+		layers.get(layers.size());
+		layers.remove(layers.size());
+	}
+
+}
